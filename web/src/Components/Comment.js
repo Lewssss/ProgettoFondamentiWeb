aec4efc9d30fcdbaf25b971a1411706410162e57
Francesco Carriero <francescocarriero94@gmail.com>
2026-09-14 13:31:50 +0200

    fix foto comments

--- web/src/Components/Comment.js
+++ web/src/Components/Comment.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useContext } from 'react';
 import {postsContext} from '../Context/PostsContext'
+import { fileUrl } from '../api/config'
 
 function Comment({comment, replies, handleCommentReply}){
     const {PublishedOn} = useContext(postsContext);
     return(
         <React.Fragment key={comment.id}>
             <div className="comment">
-                <img src={comment.author.profilePicture}/>
+                <img src={fileUrl(comment.author.profilePicture)} alt="" referrerPolicy="no-referrer" />
                 <p className="username">{comment.author.username}:</p>
                 <p className="text">{comment.text}</p>
                 <p className="reply" onClick={() => handleCommentReply(comment)}>Rispondi</p>
@@ -15,7 +16,7 @@ function Comment({comment, replies, handleCommentReply}){
             </div>
             {replies.map((commentReply) => (
                 <div key={commentReply.id} className="comment comment-replyTo">
-                    <img src={commentReply.author.profilePicture} alt="" />
+                    <img src={fileUrl(commentReply.author.profilePicture)} alt="" referrerPolicy="no-referrer" />
                     <p className="username">{commentReply.author.username}:</p>
                     <p className="text">{commentReply.text}</p>
                 </div>
